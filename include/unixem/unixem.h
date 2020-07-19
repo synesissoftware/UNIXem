f610c51cde20a1ab98dd24df9f342dcaf4cf07f1
Matt <Wilson>
2020-07-19 14:14:46 +1000

    refactored makefiles (in auto-generation)

--- include/unixem/unixem.h
+++ include/unixem/unixem.h
@@ -4,10 +4,11 @@
  * Purpose: Version header for the UNIXem API.
  *
  * Created: 29th August 2005
- * Updated: 14th October 2019
+ * Updated: 19th July 2020
  *
  * Home:    http://synesis.com.au/software/
  *
+ * Copyright (c) 2019-2020, Matthew Wilson and Synesis Information Systems
  * Copyright (c) 2005-2019, Matthew Wilson and Synesis Software
  * All rights reserved.
  *
@@ -20,9 +21,10 @@
  * - Redistributions in binary form must reproduce the above copyright
  *   notice, this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
- * - Neither the name(s) of Matthew Wilson and Synesis Software nor the
- *   names of any contributors may be used to endorse or promote products
- *   derived from this software without specific prior written permission.
+ * - Neither the name(s) of Matthew Wilson and Synesis Information Systems
+ *   nor the names of any contributors may be used to endorse or promote
+ *   products derived from this software without specific prior written
+ *   permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
@@ -52,8 +54,8 @@
 #ifndef UNIXEM_DOCUMENTATION_SKIP_SECTION
 # define UNIXEM_VER_UNIXEM_H_UNIXEM_MAJOR       1
 # define UNIXEM_VER_UNIXEM_H_UNIXEM_MINOR       7
-# define UNIXEM_VER_UNIXEM_H_UNIXEM_REVISION    2
-# define UNIXEM_VER_UNIXEM_H_UNIXEM_EDIT        26
+# define UNIXEM_VER_UNIXEM_H_UNIXEM_REVISION    3
+# define UNIXEM_VER_UNIXEM_H_UNIXEM_EDIT        27
 #endif /* !UNIXEM_DOCUMENTATION_SKIP_SECTION */
 
 /** \def UNIXEM_VER_MAJOR
@@ -74,9 +76,9 @@
 
 #define UNIXEM_VER_MAJOR       1
 #define UNIXEM_VER_MINOR       12
-#define UNIXEM_VER_REVISION    3
+#define UNIXEM_VER_REVISION    4
 
-#define UNIXEM_VER             0x010c03ff
+#define UNIXEM_VER             0x010c04ff
 
 /* ////////////////////////////////////////////////////////////////////// */
 
