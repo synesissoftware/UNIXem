ff32f55126050db0c312658d00d8a64278c49fb8
Matt Wilson <matthew@synesis.com.au>
2025-05-11 17:00:03 +1000

    **test.component.dirent** : + added more active test-cases

--- test/component/test.component.dirent/entry.cpp
+++ test/component/test.component.dirent/entry.cpp
@@ -4,6 +4,7 @@
 #include <xtests/terse-api.h>
 
 #include <stlsoft/smartptr/scoped_handle.hpp>
+#include <winstl/filesystem/filesystem_traits.hpp>
 #include <winstl/filesystem/readdir_sequence.hpp>
 
 #include <algorithm>
@@ -19,6 +20,7 @@ namespace {
 
     void TEST_readdir_CWD_FOR_DIRECTORIES_AND_FILES_AND_DOTS();
     void TEST_readdir_CWD_FOR_DIRECTORIES_AND_FILES_NO_DOTS();
+    void TEST_readdir_CWD_FOR_FILES_ONLY();
 } // anonymous namespace
 
 
@@ -33,6 +35,7 @@ int main(int argc, char** argv)
     {
         XTESTS_RUN_CASE(TEST_readdir_CWD_FOR_DIRECTORIES_AND_FILES_AND_DOTS);
         XTESTS_RUN_CASE(TEST_readdir_CWD_FOR_DIRECTORIES_AND_FILES_NO_DOTS);
+        XTESTS_RUN_CASE(TEST_readdir_CWD_FOR_FILES_ONLY);
 
         XTESTS_PRINT_RESULTS();
 
@@ -46,6 +49,7 @@ int main(int argc, char** argv)
 namespace {
 
     using winstl::readdir_sequence;
+    typedef winstl::filesystem_traits<char>                 fs_traits_t;
 
 
 void TEST_readdir_CWD_FOR_DIRECTORIES_AND_FILES_AND_DOTS()
@@ -85,7 +89,7 @@ void TEST_readdir_CWD_FOR_DIRECTORIES_AND_FILES_AND_DOTS()
 
         std::sort(rd_entries.begin(), rd_entries.end());
 
-        TEST_INT_EQ(rds_entries.size(), rd_entries.size());
+        REQUIRE(TEST_INT_EQ(rds_entries.size(), rd_entries.size()));
 
         for (size_t i = 0; rds_entries.size() != i; ++i)
         {
@@ -140,7 +144,58 @@ void TEST_readdir_CWD_FOR_DIRECTORIES_AND_FILES_NO_DOTS()
 
         std::sort(rd_entries.begin(), rd_entries.end());
 
-        TEST_INT_EQ(rds_entries.size(), rd_entries.size());
+        REQUIRE(TEST_INT_EQ(rds_entries.size(), rd_entries.size()));
+
+        for (size_t i = 0; rds_entries.size() != i; ++i)
+        {
+            TEST_MS_EQ(rds_entries[i], rd_entries[i]);
+        }
+    }
+}
+
+void TEST_readdir_CWD_FOR_FILES_ONLY()
+{
+    readdir_sequence            entries(".", readdir_sequence::files);
+    std::vector<std::string>    rds_entries(entries.begin(), entries.end());
+
+    std::sort(rds_entries.begin(), rds_entries.end());
+
+    DIR* const dir = ::opendir(".");
+
+    if (NULL == dir)
+    {
+        int const e = errno;
+
+#ifdef _MSC_VER
+# pragma warning(push)
+# pragma warning(disable : 4996)
+#endif
+
+        XTESTS_FAIL_WITH_QUALIFIER("failed to `opendir()` on '.'", strerror(e));
+
+#ifdef _MSC_VER
+# pragma warning(pop)
+#endif
+    }
+    else
+    {
+        stlsoft::scoped_handle<DIR*>    scoper(dir, ::closedir);
+
+        std::vector<std::string>        rd_entries;
+
+        for (dirent* de; NULL != (de = ::readdir(dir)); )
+        {
+            if (fs_traits_t::is_directory(de->d_name))
+            {
+                continue;
+            }
+
+            rd_entries.push_back(de->d_name);
+        }
+
+        std::sort(rd_entries.begin(), rd_entries.end());
+
+        REQUIRE(TEST_INT_EQ(rds_entries.size(), rd_entries.size()));
 
         for (size_t i = 0; rds_entries.size() != i; ++i)
         {
