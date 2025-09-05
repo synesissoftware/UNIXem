81e02afb6b4dcaff1688f501ea4e5a750b163974
Matt Wilson <matthew.wilson.synesis@gmail.com>
2025-09-05 17:42:15 +1000

    *.sh : ~ moved info-lines into ./.sis/script_info_lines.txt

--- build_cmake.sh
+++ build_cmake.sh
@@ -47,10 +47,8 @@ while [[ $# -gt 0 ]]; do
       ;;
     --help)
 
+      [ -f "$Dir/.sis/script_info_lines.txt" ] && cat "$Dir/.sis/script_info_lines.txt"
       cat << EOF
-UNIXem is a small and simple library that provides emulation of several popular Unix API functions on the Windows platform. Its primary purpose is to assist Windows programmers who are porting to Unix or are writing multi-platform code
-Copyright (c) 2019-2024, Matthew Wilson and Synesis Information Systems
-Copyright (c) 2002-2019, Matthew Wilson and Synesis Software
 Executes CMake-generated artefacts to (re)build project
 
 $ScriptPath [ ... flags/options ... ]
