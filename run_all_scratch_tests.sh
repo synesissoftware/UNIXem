1de6552082a89d3d237fb64b7a7fd2eecb16c85d
Matt Wilson <matthew.wilson.synesis@gmail.com>
2026-07-03 14:13:12 +1000

    chore: helper scripts now implemented in terms of **/.sis/script_info_lines.txt**

--- run_all_scratch_tests.sh
+++ run_all_scratch_tests.sh
@@ -26,10 +26,8 @@ while [[ $# -gt 0 ]]; do
       ;;
     --help)
 
+      [ -f "$Dir/.sis/script_info_lines.txt" ] && cat "$Dir/.sis/script_info_lines.txt"
       cat << EOF
-UNIXem is a small and simple library that provides emulation of several popular Unix API functions on the Windows platform. Its primary purpose is to assist Windows programmers who are porting to Unix or are writing multi-platform code
-Copyright (c) 2019-2024, Matthew Wilson and Synesis Information Systems
-Copyright (c) 2002-2019, Matthew Wilson and Synesis Software
 Runs all (matching) scratch-test programs
 
 $ScriptPath [ ... flags/options ... ]
